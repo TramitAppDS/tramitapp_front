e0482726276f82c08f373b5e8d715ec7d99b1d97
Matias Mackenna <matiasmackennad@uc.cl>
2022-05-29 20:56:23 -0400

    fix(minor): lint and others

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -3,45 +3,46 @@ import React from "react";
 export default function Profile() {
   return (
     <div style={{ textAlign: "center" }}>
-      <br></br>
-      <h1 class="title is-2"> Perfil de nombre </h1>
+      <br />
+      <h1 className="title is-2"> Perfil de nombre </h1>
       <p>
         <img
+          alt=""
           src="default_avatar.png"
           style={{ width: "200px", height: "200px", borderRadius: "50%", objectFit: "fill" }}
         />
       </p>
 
-      <p class="is-size-5">
+      <p className="is-size-5">
         <strong>Calificación: </strong>
       </p>
 
-      <br></br>
-      <br></br>
+      <br />
+      <br />
 
       <div className="columns">
         <div className="column">
-          <p class="is-size-4">
+          <p className="is-size-4">
             <strong>Este mes</strong>
           </p>
 
-          <p class="is-size-5">Trámites realizados: 12</p>
+          <p className="is-size-5">Trámites realizados: 12</p>
 
-          <p class="is-size-5">Ganancias: 48.000 CLP</p>
+          <p className="is-size-5">Ganancias: 48.000 CLP</p>
 
-          <p class="is-size-5">Tiempo promedio: 40 minutos/trámite</p>
+          <p className="is-size-5">Tiempo promedio: 40 minutos/trámite</p>
         </div>
 
         <div className="column">
-          <p class="is-size-4">
+          <p className="is-size-4">
             <strong>Histórico</strong>
           </p>
 
-          <p class="is-size-5">Trámites realizados: 24</p>
+          <p className="is-size-5">Trámites realizados: 24</p>
 
-          <p class="is-size-5">Ganancias: 96.000 CLP</p>
+          <p className="is-size-5">Ganancias: 96.000 CLP</p>
 
-          <p class="is-size-5">Tiempo promedio: 45 minutos/trámite</p>
+          <p className="is-size-5">Tiempo promedio: 45 minutos/trámite</p>
         </div>
       </div>
     </div>
